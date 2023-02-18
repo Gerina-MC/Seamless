ab54ca6aaaf6bf9df302c2e7df892e91c763a4c4
Gerina-MC <gerinamary7133@gmail.com>
2023-02-18 14:38:15 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid" >
+                    <div class="navbar-brand me-auto" style="max-width:20%">
+                        <h1>Seamless</h1>
+                    </div>
+                <div>
+                    <a class="navbar-brand">Home</a>
+                    <a class="navbar-brand">Browse</a>
+                    <a class="navbar-brand">About</a>
+                </div>
+                <div class="nav navbar-nav navbar-right">
+                    <button class="btn btn-primary" type="submit">Login</button>
+                </div>
+            </div>
+        </nav>
+    </body>
+</html>
