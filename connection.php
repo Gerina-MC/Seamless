ef47dd53499a50422c73912bc58c9cdf45b1b3eb
Gerina-MC <gerinamary7133@gmail.com>
2023-03-03 17:23:41 +0530

    Changed link format

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
     error_reporting(0);      
-    $host = "sql306.epizy.com";  
-    $user = "epiz_33713201";  
-    $password = 'jem0zCeeeL4T';  
-    $db_name = "epiz_33713201_seamless";  
+    $host = "localhost";  
+    $user = "root";  
+    $password = '';  
+    $db_name = "seamless";  
       
     $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
